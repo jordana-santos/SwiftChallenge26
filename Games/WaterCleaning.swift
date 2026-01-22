141d7cdafdfaa0aa396c8ad4fb16181ce187c33d
jordana-santos <jordanalsantos@hotmail.com>
2026-01-22 15:29:23 -0300

    first attempt water cleaning game mechanics

--- Games/WaterCleaning.swift
+++ Games/WaterCleaning.swift
@@ -9,9 +9,72 @@ import SwiftUI
 
 struct waterCleaning: View {
     @Binding var path: [Route]
+    @State private var dragOffset: CGSize = .zero
+    @State private var dragOffset2: CGSize = .zero
+    @State var position = CGSize.zero
+    @State var isDragging = false
+
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { location in
+                dragOffset = location.translation
+                isDragging = true
+                
+            }
+            .onEnded { _ in
+                position.width += dragOffset.width
+                position.height += dragOffset.height
+                dragOffset = position
+                isDragging = false
+            }
+    }
+    
+    var dragLixo2: some Gesture {
+        DragGesture()
+            .onChanged { location in
+                dragOffset2 = location.translation
+                isDragging = true
+            }
+            .onEnded { _ in
+                position.width += dragOffset.width
+                position.height += dragOffset.height
+                dragOffset2 = position
+                isDragging = false
+            }
+    }
     
     var body: some View {
-        Text("Water Cleaning")
+        VStack(){
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.yellow)
+                .offset(x: 82 + dragOffset.width, y: -51 + dragOffset.height)
+                .gesture(drag)
+            
+                
+            
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.purple)
+                .offset(x: 0 + dragOffset2.width, y: 0 + dragOffset2.height)
+                .gesture(dragLixo2)
+
+            
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.orange)
+                .offset(x: -54 + dragOffset.width, y: 134 + dragOffset.height)
+                .gesture(drag)
+            
+        }
+        .navigationTitle(Text("Water Cleaning"))
+        Spacer()
+        
+        Rectangle()
+            .frame(width: 100, height: 100)
+            .foregroundColor(.gray)
+            .offset(x: 130, y: 0)
         
         Button {
             path.append(.map)
@@ -30,6 +93,15 @@ struct waterCleaning: View {
 
 
 /*
+ 
+ criar uma struct q reaproveite o codigo do drag
+ ela retorna um gesture
+ usar variaveis que recebe dessa struct
+ usar essas variaveis atraves do @biding
+ colocar um @state na struct 
+ 
+ 
+ 
  O fluxo normalmente é:
      Usuário toca no objeto
      Arrasta → você atualiza offset em tempo real
